4469609b3aefaf8361950f5700f246f558ab2c82
Justin Pilla <pillaj0@students.rowan.edu>
2016-12-07 22:55:13 -0500

    Init app

---
+++ RUQuiet/app/src/main/java/edu/rowan/jpilla/ruquiet/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.rowan.jpilla.ruquiet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
