2acd2fe56396b0a325445431e433ee887ac4f22b
Justin Pilla <pillaj0@students.rowan.edu>
2016-12-12 13:46:30 -0500

    +Rowan Hall, sorted buildings

--- RUQuiet/app/src/main/java/edu/rowan/jpilla/ruquiet/MainActivity.java
+++ RUQuiet/app/src/main/java/edu/rowan/jpilla/ruquiet/MainActivity.java
@@ -18,6 +18,8 @@ import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.model.LatLng;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class MainActivity extends Activity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
@@ -63,8 +65,13 @@ public class MainActivity extends Activity implements GoogleApiClient.Connection
         buildingList.add(new Building("Robinson Hall",new LatLng(39.710542, -75.120683), new LatLng(39.710309, -75.119969), new LatLng(39.710623, -75.119795), new LatLng(39.710858, -75.120519)));
         buildingList.add(new Building("Science Hall", new LatLng(39.71019,-75.120958), new LatLng(39.70967,-75.121215), new LatLng(39.709369,-75.120287), new LatLng(39.709889,-75.120024), new LatLng(39.709885,-75.120362), new LatLng(39.709782,-75.120437), new LatLng(39.709893,-75.120609), new LatLng(39.709918,-75.120765), new LatLng(39.710108,-75.120663)));
         buildingList.add(new Building("James Hall", new LatLng(39.712064,-75.119936), new LatLng(39.711676,-75.120145), new LatLng(39.711338,-75.119083), new LatLng(39.711726,-75.118847)));
-
-
+        buildingList.add(new Building("Rowan Hall", new LatLng(39.712551,-75.122701), new LatLng(39.712448,-75.122803), new LatLng(39.712093,-75.122412), new LatLng(39.711676,-75.122589), new LatLng(39.711618,-75.122342), new LatLng(39.712365,-75.121897), new LatLng(39.712489,-75.122267), new LatLng(39.712303,-75.122358)));
+        Collections.sort(buildingList, new Comparator<Building>() {
+            @Override
+            public int compare(Building lhs, Building rhs) {
+                return lhs.getName().compareTo(rhs.getName());
+            }
+        });
     }
 
     @Override
